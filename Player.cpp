e8a4f2075a685d668104cab0a3afd2ce449f925b
野村優羽 <@MSI>
2023-04-17 17:09:57 +0900

    スライド16まで

---
+++ Player.cpp
@@ -0,0 +1,19 @@
+#include "Player.h"
+
+void Player::Initialize() {
+
+
+
+}
+
+void Player::Update() {
+
+
+
+}
+
+void Player::Draw() {
+
+
+
+}
---
+++ Player.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//---自キャラクラス---//
+class Player {
+
+	public:
+		//初期化
+	    void Initialize();
+		//更新
+	    void Update();
+		//描画
+	    void Draw();
+
+};

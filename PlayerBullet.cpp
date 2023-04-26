ba6b010dec0f9b682ef5fcaded43619a3ff82bbb
野村優羽 <@MSI>
2023-04-26 16:42:36 +0900

    完了

--- PlayerBullet.cpp
+++ PlayerBullet.cpp
@@ -1,7 +1,7 @@
 #include "PlayerBullet.h"
 #include <cassert>
 
-void PlayerBullet::Initialize(Model* model, const Vector3& position) {
+void PlayerBullet::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
 
 	//NULLポインタチェック
 	assert(model);
@@ -14,11 +14,23 @@ void PlayerBullet::Initialize(Model* model, const Vector3& position) {
 	//引数で受け取った初期座標をセット
 	worldTransform_.translation_ = {position.x, position.y, position.z};
 
+	//引数で受け取った速度をメンバ変数に代入
+	velocity_ = velocity;
+
 }
 
 void PlayerBullet::Update() {
 
+	//座標を移動させる(1フレーム分の移動量を差し込む)
+	worldTransform_.translation_ = Add(worldTransform_.translation_, velocity_);
+
+	//行列を更新
 	worldTransform_.UpdateMatrix();
+
+	//時間経過でデス
+	if (--deathTimer_ <= 0) {
+		isDead_ = true;
+	}
 
 }
 

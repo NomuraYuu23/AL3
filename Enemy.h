c753e3f34e1c6ef5999d638067f90ed204dfd720
野村優羽 <@MSI>
2023-04-28 09:59:11 +0900

    ヘッダ作成まで

--- Enemy.h
+++ Enemy.h
@@ -2,9 +2,34 @@
 #include "Model.h"
 // #include "WorldTransform.h"
 
+//前方宣言
+class Enemy;
+
+class BaseEnemyState {
+public:
+	virtual void Update() = 0;
+
+};
+
+class EnemyStateApproach : public BaseEnemyState {
+public:
+	void Update();
+};
+
+class EnemyStateLeave : public BaseEnemyState {
+public:
+	void Update();
+};
+
 class Enemy {
 
 	public:
+
+	//コンストラクタ
+	Enemy();
+	//デストラクタ
+	~Enemy();
+
 	// 初期化
 	void Initialize(Model* model, uint32_t textureHandle);
 	// 更新
@@ -12,10 +37,7 @@ class Enemy {
 	// 描画
 	void Draw(ViewProjection viewProjection);
 
-	//接近フェーズ
-	void Approach();
-	//離脱フェーズ
-	void Leave();
+	void ChangeState(BaseEnemyState* newState);
 
 	private:
 	// ワールド変換データ
@@ -24,14 +46,6 @@ class Enemy {
 	Model* model_ = nullptr;
 	// テクスチャハンドル
 	uint32_t textureHandle_ = 0u;
-
-	//行動フェーズ
-	enum class Phase {
-		Approach, //接近する
-		Leave,    //離脱する
-	};
-
-	Phase phase_ = Phase::Approach;
 
 	Vector3 velocity = {
 	    0.0f,

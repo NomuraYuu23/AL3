ca35e3436e828deda5949ca5997135893fb186bc
野村優羽 <@MSI>
2023-05-01 10:53:37 +0900

    完了

--- EnemyBullet.cpp
+++ EnemyBullet.cpp
@@ -1,5 +1,6 @@
 #include "EnemyBullet.h"
 #include <cassert>
+#include <cmath>
 
 // 初期化
 void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
@@ -17,6 +18,17 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector
 	//引数で受け取った速度をメンバ変数に代入
 	velocity_ = velocity;
 
+	//Z方向に伸びた形状
+	worldTransform_.scale_.x = 0.5f;
+	worldTransform_.scale_.y = 0.5f;
+	worldTransform_.scale_.z = 3.0f;
+	
+	//Y軸周りの角度(Θy)
+	worldTransform_.rotation_.y = std::atan2f(velocity_.x, velocity_.z);
+	//横軸方向の長さを求める
+	float length = Length(Vector3{velocity_.x, 0.0f, velocity_.z});
+	//X軸周りの角度(Θx)
+	worldTransform_.rotation_.x = std::atan2f(-velocity_.y, length);
 
 }
 

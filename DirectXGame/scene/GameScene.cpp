1e1888af8ecb5935aa43d993ed8e44594ea55d5e
野村優羽 <@MSI>
2023-07-05 13:59:03 +0900

    軸

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -17,6 +17,11 @@ void GameScene::Initialize() {
 
 	viewProjection_.Initialize();
 
+	// 軸方向表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	// 軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&viewProjection_);
+
 	//デバッグカメラ
 	debugCamera_ = std::make_unique<DebugCamera>(1280, 720);
 

b08082f02a4fc579d510a8975460cf5ec6c29977
野村優羽 <@MSI>
2023-07-08 18:39:28 +0900

    スライド35

--- DirectXGame/FollowCamera.cpp
+++ DirectXGame/FollowCamera.cpp
@@ -1,5 +1,8 @@
 #include "FollowCamera.h"
 #include"Vector3Calc.h"
+#include "Matrix4x4Calc.h"
+#include "input.h"
+#include <Xinput.h>
 
 void FollowCamera::Initialize() {
 
@@ -9,14 +12,35 @@ void FollowCamera::Initialize() {
 
 void FollowCamera::Update() {
 
+	XINPUT_STATE joyState;
+
 	//追従対象がいれば
 	if (target_) {
 		//追従対象からカメラまでのオフセット
 		Vector3 offset = {0.0f, 2.0f, -20.0f};
 
+		//カメラの角度から回転行列を計算する
+		Matrix4x4 rotateMatrixX = Matrix4x4Calc::MakeRotateXMatrix(viewProjection_.rotation_.x);
+		Matrix4x4 rotateMatrixY = Matrix4x4Calc::MakeRotateYMatrix(viewProjection_.rotation_.y);
+		Matrix4x4 rotateMatrixZ = Matrix4x4Calc::MakeRotateZMatrix(viewProjection_.rotation_.z);
+
+		Matrix4x4 rotateMatrix = Matrix4x4Calc::Multiply(
+		    rotateMatrixX, Matrix4x4Calc::Multiply(rotateMatrixY, rotateMatrixZ));
+
+		//オフセットをカメラの回転に合わせて回転させる
+		offset = Matrix4x4Calc::TransformNormal(offset, rotateMatrix);
+
 		//座標をコピーしてオフセット分ずらす
 		viewProjection_.translation_ = Vector3Calc::Add(target_->translation_, offset);
 
+	}
+
+	if (Input::GetInstance()->GetJoystickState(0, joyState)) {
+	
+		const float RotateSpeed = 0.000001f;
+
+		viewProjection_.rotation_.y += float(joyState.Gamepad.sThumbRX) * RotateSpeed;
+	
 	}
 
 	//ビュー更新
